f2b728d305d28ac798fbf8ad172c058765f6f4f0
j-evs <jan.evseev@gmail.com>
2019-05-17 22:27:29 +0300

    bootstrap redux and react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>Hi from Index</h2>;
+}
+
+function SubredditInfo({
+  match: {
+    params: { subreddit }
+  }
+}) {
+  return <h2>Hi from {subreddit}</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/askReddit">Askreddit link</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/about/:subreddit" component={SubredditInfo} />
+      </div>
+    </Router>
   );
 }
 

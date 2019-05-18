bf3973c14c9f0221b5784e3e6ae52141897831c8
j-evs <jan.evseev@gmail.com>
2019-05-18 21:40:22 +0300

    Change loader

--- src/components/common/Loader/Loader.js
+++ src/components/common/Loader/Loader.js
@@ -4,9 +4,9 @@ import styles from "./Loader.module.css";
 
 const Loader = () => (
   <div className={styles.loader}>
-    <div />
-    <div />
-    <div />
+    <svg viewBox="25 25 50 50">
+      <circle cx="50" cy="50" r="20" />
+    </svg>
   </div>
 );
 
